6b28ecc71e61715bc9c57248c109734fe8d03b27
Dakota Vikdal <dakota.james19@hotmail.com>
2024-05-19 14:06:13 -0400

    finished up the projects section of the website

--- portfolio2/src/projects.js
+++ portfolio2/src/projects.js
@@ -1,47 +1,54 @@
+import image1 from './assets/projectCards/Blockbuster.png'
+import image2 from './assets/projectCards/Free-Games-Finder.png'
+import image3 from './assets/projectCards/Image-Converter.png'
+import image4 from './assets/projectCards/PDF-Playground.jpg'
+import image5 from './assets/projectCards/webscraping.jpg'
+import image6 from './assets/projectCards/minigames.jpg'
 
-// figure out why the images don't render when you utilize the path ../assets/
+
+
 const projects = [
     {
       id: 1,
       name: "Blockbuster Employee Portal",
-      about: "Are you a blockbuster employee? If so you're in luck! This application allows you to add, update and delete customers and movies from a database. It also allows you to rent said movies to said customers. What fun!",
+      about: "Are you a Blockbuster employee? If so, you're in luck! This application allows you to add, update, and delete customers and movies from a database. It also enables you to rent movies to customers. What fun!",
       link: "https://github.com/Dakota-Vikdal/blockbuster-employee-portal",
-      image: "https://i.postimg.cc/qvKRCq7p/Blockubuster-Snapshot.png",
+      image: image1,
     },
     {
       id: 2,
       name: "Free Games Finder!",
-      about: "Have you ever wanted to play a videogame but didn't want to spend any money? Look no further than the free games finder. This application showcases hundreds of free games you can find around the web.",
+      about: "Have you ever wanted to play a video game but didn't want to spend any money? Look no further than the Free Games Finder! This application showcases hundreds of free games you can find around the web.",
       link: "https://github.com/Dakota-Vikdal/Free-Games-Project",
-      image: "https://i.postimg.cc/j2ST270M/Free-Games-Finder-Screenshot.png",
+      image: image2,
     },
     {
       id: 3,
       name: "Image Converter",
-      about: "This is a Python script that converts JPG images into PNG images. I was clearly bored and decided to tinker.",
+      about: "This is a Python script that converts JPG images into PNGs. I can't think of a more enjoyable pastime than converting my images from JPG to PNG. Can you? Didn't think so!",
       link: "https://github.com/Dakota-Vikdal/convert-images",
-      image: "https://i.postimg.cc/0QY84vPq/Image-Converter.png",
+      image: image3,
     },
     {
       id: 4,
       name: "PDF Playground!",
-      about: "Have you ever wanted to play a videogame but didn't want to spend any money? Look no further than the free games finder. This application showcases hundreds of free games you can find around the web.",
+      about: "A Python script that allows you to add watermarks to your PDF files! WHHHATTTTT!!!!! THAT'S SO COOL!",
       link: "https://github.com/Dakota-Vikdal/Pdf_playground",
-      image: "../assets/pdf.png",
+      image: image4,
     },
     {
       id: 5,
       name: "Beautiful Scraping!",
-      about: "Have you ever wanted to play a videogame but didn't want to spend any money? Look no further than the free games finder. This application showcases hundreds of free games you can find around the web.",
+      about: "In this magnificent script we are able to collect only the essential data from a particular website. Comes in handy if you're trying to save a few seconds.",
       link: "https://github.com/Dakota-Vikdal/beautifulsoup_scraping",
-      image: "",
+      image: image5,
     },
     {
       id: 6,
       name: "Mini games!",
-      about: "Have you ever wanted to play a videogame but didn't want to spend any money? Look no further than the free games finder. This application showcases hundreds of free games you can find around the web.",
+      about: "Ever find yourself with freetime? Not sure what to do with it? Dakota's got you covered! Here we've got minigames that will keep you occupied for several minutes! Maybe even up to 5! Whoot whoot! Happy playing!",
       link: "https://github.com/Dakota-Vikdal/Mini-Projects",
-      image: "",
+      image: image6,
     },
 ]
 
